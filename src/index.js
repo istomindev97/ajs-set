f132d4ed32fa079b41b5c9728b61690db91446e5
Ilya Istomin <istominilya97@gmail.com>
2024-09-02 16:18:35 +0300

    initial commit

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import './css/style.css';
+
+import  console  from './js/set';
+
+import  Character  from './js/character';
+
+import  Bowman  from './js/bowman';
+
+import  Daemon  from './js/daemon';
+
+import  Magician  from './js/magician';
+
+import  Swordsman  from './js/swordsman';
+
+import  Undead  from './js/undead';
+
+import  Zombie  from './js/zombie';
+
+
+const vasya = new Zombie ('Вася')
+const shortname = new Undead ('I');
+
+console.log(console);
+
+console.log(vasya);
+console.log(shortname);
+
